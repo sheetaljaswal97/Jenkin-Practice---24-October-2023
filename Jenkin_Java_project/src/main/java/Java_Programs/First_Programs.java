f98ac75e5c5b80e8c24c516b05cdb40504c3c6c1
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-10-24 21:02:19 +0530

    second commit

--- Jenkin_Java_project/src/main/java/Java_Programs/First_Programs.java
+++ Jenkin_Java_project/src/main/java/Java_Programs/First_Programs.java
@@ -1,10 +1,21 @@
 package Java_Programs;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
 public class First_Programs {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Test
+	public void method() throws InterruptedException
+	{
+		
+
+		System.out.println("Testng jenkin class");
+        
 
 	}
-
 }
