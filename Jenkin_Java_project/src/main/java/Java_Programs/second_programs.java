cee34cd2274f980b64399ccd5fa36f02b8c76ffe
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-11-29 19:34:21 +0530

    first commit

---
+++ Jenkin_Java_project/src/main/java/Java_Programs/second_programs.java
@@ -0,0 +1,11 @@
+package Java_Programs;
+
+public class second_programs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("second programs");
+
+	}
+
+}
